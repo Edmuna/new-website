a311a91ea27077690b92f25274aad0d13f56037d
Edmuna <ereda.hr@gmail.com>
2023-09-29 11:25:08 +0200

    almost done

--- script.js
+++ script.js
@@ -18,3 +18,69 @@ if (window.innerWidth <= 768) {
 
     splide.mount();
 }
+
+// const form = document.getElementById('myForm');
+// const sender = document.getElementById("name");
+// const email = document.getElementById("email");
+// const msg = document.getElementById("message");
+
+// form.addEventListener('submit', function (event) {
+//     event.preventDefault();
+
+//     const templateParams = {
+//         sender: sender.value,
+//         email: email.value,
+//         message: msg.value,
+//     };
+
+//     emailjs.send('service_90nreuh', 'template_wle2anc', templateParams, "1iqi5gSawssZ1Xp36")
+//         .then(function (response) {
+//             console.log('SUCCESS!', response.status, response.text, response, templateParams);
+//             sender.value = "";
+//             email.value = "";
+//             msg.value = "";
+//             // Here the content of the button should be changed or some notification that the e-mail has been succesfully sent
+//         })
+//         .catch(function (error) {
+//             console.log('FAILED...', error);
+//         });
+// });
+
+const form = document.getElementById('myForm');
+const sender = document.getElementById("name");
+const email = document.getElementById("email");
+const msg = document.getElementById("message");
+const submitButton = document.querySelector(".btn"); // Select the submit button
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const templateParams = {
+        sender: sender.value,
+        email: email.value,
+        message: msg.value,
+    };
+
+    // Disable the submit button while sending the email
+    submitButton.disabled = true;
+    submitButton.textContent = "Sending..."; // Update the button text
+
+    emailjs.send('service_90nreuh', 'template_r0y7ias', templateParams, "1iqi5gSawssZ1Xp36")
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text, response, templateParams);
+            sender.value = "";
+            email.value = "";
+            msg.value = "";
+            // Here, you can update the button text or display a success message.
+            submitButton.textContent = "Message Sent!";
+        })
+        .catch(function (error) {
+            console.log('FAILED...', error);
+            // Here, you can update the button text or display an error message.
+            submitButton.textContent = "Error Sending Message";
+        })
+        .finally(function () {
+            // Re-enable the submit button after the operation is complete
+            submitButton.disabled = false;
+        });
+});
